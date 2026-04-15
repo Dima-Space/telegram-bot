081d564168d17c98cca0155c44a46a8ad5d91c07
Dima-Space <dimamalchik240@gmail.com>
2026-04-15 16:34:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = "8408563049:AAFdnf7mrkPCsJi2pXLVSadsqQl0oZITKCw"
+TOKEN = "8408563049:AAHi08540D57PJW15e_Sqcy992MFuNtFXIo"
 NIGHT_START = 23
 NIGHT_END = 8
 
